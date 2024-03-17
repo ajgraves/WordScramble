71a23bb3c7354b6d6bf0d0da63be86cb98a08d7e
Aaron Graves <ajgraves@ajgraves.com>
2024-03-17 12:48:52 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Aaron Graves on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
